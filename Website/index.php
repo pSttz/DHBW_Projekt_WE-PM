a58a5b5a9b7b321db9f0ef6da5a7a4bfad87841d
cmmdmx <m@ixdee.de>
2015-05-28 20:19:58 +0200

    Sample Content
    Die allerwichtigsten Funktionen von php

--- Website/index.php
+++ Website/index.php
@@ -1,3 +1,12 @@
+<?php
+	session_start();
+
+	$content = mysql_connect("localhost", "skymap",  "u6&bNl58") or die (mysql_error());
+	mysql_select_db("skymap") or die (mysql_error());
+	
+	
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,6 +17,21 @@
 
 </head>
 <body>
+	<?php
+	$link = $_GET['link'];
+	?>
 
+	<div>
+		<?php 
+			//include("menu.php");
+			
+			$item = mysql_query("SELECT * FROM galerie");
+			while($data = mysql_fetch_array($item))
+			{
+				echo($data[1]."<img src='".$data[2]."' width='100px'><br />");
+			}
+			
+		?>
+	</div>
 </body>
 </html>
