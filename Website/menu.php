793d7b4d68e0e51f7ca79875daf469cb2f935b5c
pSttz <patrick-stuetz@gmx.de>
2015-05-28 21:42:30 +0200

    update flexlayout

---
+++ Website/menu.php
@@ -0,0 +1,8 @@
+<div class="flexparent flexmenu">
+	<div class="menu">
+		<div class="menuitem">Startseite</div>
+		<div class="menuitem">Galerie</div>
+		<div class="menuitem">Angebote</div>
+		<div class="menuitem">Kontakt</div>
+	</div>
+</div>
