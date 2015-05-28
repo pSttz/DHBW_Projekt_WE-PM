b0ee153829b58978b86ae90d575aec86a53545e2
pSttz <patrick-stuetz@gmx.de>
2015-05-28 19:28:26 +0200

    Add files and import to index.php

---
+++ Website/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+	<script type="text/javascript" src="script/jquery-1.11.2.js"></script>
+	<script type="text/javascript" src="script/script.js"></script>
+
+</head>
+<body>
+
+</body>
+</html>
