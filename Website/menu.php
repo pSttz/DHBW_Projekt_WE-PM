d3b36e5325cf6cebb5dfbffa696d77b5a8953d73
ygonzalo <yaiza.alt@gmail.com>
2015-06-02 22:37:55 +0200

    Menu bar links, colors corrected

--- Website/menu.php
+++ Website/menu.php
@@ -1,8 +1,8 @@
 <div class="flexparent flexmenu">
 	
-		<div class="menuitem">HOME</div>
-		<div class="menuitem">GALERIE</div>
-		<div class="menuitem">ANGEBOT</div>
-		<div class="menuitem">KONTAKT</div>
+		<div class="menuitem"><a href='?p=home'>HOME</a></div>
+		<div class="menuitem"><a href='?p=gallery'>GALERIE</a></div>
+		<div class="menuitem"><a href='?p=products'>ANGEBOT</a></div>
+		<div class="menuitem"><a href='?p=contact'>KONTAKT</a></div>
 	
 </div>
