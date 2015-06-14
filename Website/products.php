42a863560635dd993592fe8f799e0cd81259ee43
pSttz <patrick-stuetz@gmx.de>
2015-06-14 23:01:30 +0200

    Add Content to products.php
    Added the Information out of Pricelist.pdf
    styled the page a little

--- Website/products.php
+++ Website/products.php
@@ -19,7 +19,7 @@ Unsere Leistungen:
 <div class="click link" onclick="toggle_visibility('passbilder');" >&raquo; Passbilder</div>
 	<div id="passbilder" style='display:none;'>
 		
-		<table id="passbildContent">
+		<table class="linkContent">
 			<tr>
 				<td class="tableLeft">
 					Biometrische Passbilder (4 St&uuml;ck)<br>
@@ -45,26 +45,83 @@ Unsere Leistungen:
 <br>
 
 <div class="click link" onclick="toggle_visibility('fotoshootings');">&raquo; Fotoshootings</div>
-  <div id="fotoshootings" style="display:none;">
- 	<em>Kleines Fotoshooting</em><br>
- 	<em>Großes Fotoshooting</em>
-  </div>
+  	<div id="fotoshootings" style="display:none;">
+ 		<table class="linkContent">
+ 			<tr colspan="2">
+ 				<td class="focus">Kleines Fotoshooting</td>
+ 			</tr>
+ 			<tr>
+ 				<td class="tableleft">
+ 					- Dauer: 1 Stunde, inklusive Beratungsgespr&auml;ch<br>
+ 					- 2 verschiedene Outfits<br>
+ 					- Auf Wunsch auch Outdoor-Shooting<br>
+ 					- Gemeinsames Erarbeiten des optimalen Fotos<br>
+ 					- permanente Begutachtung der Fotos am Bildschirm<br>
+ 					- inklusive aller Fotos in hoher Auflösung auf einer DVD<br>
+ 					- 3 Top-Fotos werden digital optimiert und auf Wunsch in Schwarz-Wei&szlig; umgewandelt<br>
+ 					- kleine Hautirritationen werden wegretuschiert (z.B. Pickel, Herpesbläschen)<br>
+ 					- Nutzungsrechte für Facebook, Instagram und andre Social-Media-Plattformen<br>
+ 				</td>
+ 				<td>
+	 				150,00 &euro;	
+ 				</td>
+ 			</tr>
+ 			<tr>
+ 				<td class="focus">Gro&szlig;es Fotoshooting</td>
+ 			</tr>
+ 			<tr>
+ 				<td class="tableleft">
+ 					- Dauer: 2 Stunden, inklusive Beratungsgespr&auml;ch<br>
+ 					- 4 verschiedene Outfits<br>
+ 					- Auf Wunsch auch Outdoor-Shooting<br>
+ 					- Gemeinsames Erarbeiten des optimalen Fotos<br>
+ 					- Permanente Begutachtung der Fotos am Bildschirm<br>
+ 					- inklusive aller Fotos in hoher Auflösung auf einer DVD<br>
+ 					- 6 Top-Fotos werden digital optimiert und auf Wunsch in Schwarz-Wei&szlig; umgewandelt<br>
+ 					- kleine Hautirritationen werden Wegretuschiert (z.B. Pickel, Herpesbläschen)<br>
+ 					- Nutzungsrechte für Facebook, Instagram und andere Social-Media-Plattformen<br>
+ 				</td>
+ 				<td>250,00 &euro;</td>
+ 			</tr>
+ 		</table>
+
+
+ 	</div>
 <br>
 
 <div class="click link" onclick="toggle_visibility('eventfoto');">&raquo; Eventfotografie</div>
-  <div id="eventfoto" style="display: none;">
- <em>Kleines Fotoshooting</em><br>
- <em>Großes Fotoshooting</em>
-</div>
+	<div id="eventfoto" style="display: none;">
+ 		<table class="linkContent">
+ 			<tr colspan="2">
+ 				<td class="focus">Eventpaket</td>
+ 			</tr>
+ 			<tr>
+ 				
+ 				<td class="tableleft">
+ 					- 8 Stunden Eventfotografie, inklusive aller Fotos auf DVD<br>
+ 					- Reportage, ca. 1.500 Fotos<br>
+ 					- inklusive 50 High-End-Fotobearbeitungen von 25 Motiven<br>
+ 					- Anreise bis zu 50km ab meinem Studio frei, danach 0,95 &euro; pro Kilometer<br>
+ 					- Verlängerungsstunde: 150,00 &euro;<br>
+ 				</td>
+ 				<td>1.500,00 &euro;</td>
+ 			</tr>
+ 		</table>
+
+	</div>
 
 <br>
 
 <form action="download.php?file=skymap_fotostudio.pdf" method="POST">
-   SKYMAP Fotostudio Preisliste als PDF Datei: <input type="submit" value="Download">
+   <p>Unsere gesamte Preisliste können Sie sich auch gerne im PDF-Format heruntertladen<br>
+   	<input type="submit" value="Download">
+   </p>
+
 </form>
 <h3>Business-Kunden</h3>
-Zeit für ein neues Image? Wir bieten Ihnen profesionelle Hilfe bei der Gestaltung von Medien für Ihr Unternehmen.
-
+<p class="slogan">Zeit für ein neues Image? Wir bieten Ihnen profesionelle Hilfe bei der Gestaltung von Medien für Ihr Unternehmen.<br>
+Bitte kontaktieren Sie uns und wir werden gemeinsam Ihre Ziele und W&uuml;nsche umsetzen.
+</p>
 <h2>Fotoservice</h2>
 
 
