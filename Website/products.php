260a56c784e245cd6429a8693f44fbab4afec1ce
pSttz <patrick-stuetz@gmx.de>
2015-06-14 18:15:15 +0200

    change function of links

--- Website/products.php
+++ Website/products.php
@@ -16,27 +16,27 @@
 Ob für sich selbst oder als Geschenk, bei SKYMAP werden Ihre individuelle Fotowünsche umgesetzt.
 Unsere Leistungen:
 <br>
-<div class="click" onclick="toggle_visibility('passbilder');" >> Passbilder</div>
+<div class="click link" onclick="toggle_visibility('passbilder');" >&raquo; Passbilder</div>
 <div id="passbilder" style='display:none;'>qetqert</div>
 <br>
 
 
 
-<div id="topbox">
-	<a> Fotoshootings</a><br>
-  <div id="tophiddenbox">
- <em>Kleines Fotoshooting</em><br>
- <em>Großes Fotoshooting</em>
-</div>
-</div>
-<div id="topbox">
-	<a>> Eventfotografie</a><br>
-  <div id="tophiddenbox">
- <em>Kleines Fotoshooting</em><br>
- <em>Großes Fotoshooting</em>
-</div>
-</div>
+<div class="click link" onclick="toggle_visibility('fotoshootings');">&raquo; Fotoshootings</div>
+  <div id="fotoshootings" style="display:none;">
+ 	<em>Kleines Fotoshooting</em><br>
+ 	<em>Großes Fotoshooting</em>
+  </div>
 <br>
+
+<div class="click link" onclick="toggle_visibility('eventfoto');">&raquo; Eventfotografie</div>
+  <div id="eventfoto" style="display: none;">
+ <em>Kleines Fotoshooting</em><br>
+ <em>Großes Fotoshooting</em>
+</div>
+
+<br>
+
 <form action="download.php?file=skymap_fotostudio.pdf" method="POST">
    SKYMAP Fotostudio Preisliste als PDF Datei: <input type="submit" value="Download">
 </form>
