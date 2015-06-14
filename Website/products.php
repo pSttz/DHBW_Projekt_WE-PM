dfb406815950fc5b2fb472e7eece9e74e4c5478e
ygonzalo <yaiza.alt@gmail.com>
2015-06-14 17:56:50 +0200

    Product page dropdown

--- Website/products.php
+++ Website/products.php
@@ -1,17 +1,29 @@
+<script type="text/javascript">
+<!--
+    function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
+//-->
+</script>
+
 <h1>Angebote</h1>
 <h2>Fotostudio</h2>
 <h3>Privat-Kunden</h3>
 Ob für sich selbst oder als Geschenk, bei SKYMAP werden Ihre individuelle Fotowünsche umgesetzt.
 Unsere Leistungen:
+<br>
+<div class="click" onclick="toggle_visibility('passbilder');" >> Passbilder</div>
+<div id="passbilder" style='display:none;'>qetqert</div>
+<br>
+
+
+
 <div id="topbox">
-	<a>> Passbilder</a><br>
-  <div id="tophiddenbox">
- <em>Kleines Fotoshooting</em><br>
- <em>Großes Fotoshooting</em>
-</div>
-</div>
-<div id="topbox">
-	<a>> Fotoshootings</a><br>
+	<a> Fotoshootings</a><br>
   <div id="tophiddenbox">
  <em>Kleines Fotoshooting</em><br>
  <em>Großes Fotoshooting</em>
@@ -32,4 +44,5 @@ Unsere Leistungen:
 Zeit für ein neues Image? Wir bieten Ihnen profesionelle Hilfe bei der Gestaltung von Medien für Ihr Unternehmen.
 
 <h2>Fotoservice</h2>
+
 
