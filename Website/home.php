4c0aa355df1d1db218e483468d77c4db66368386
SKicherer <Sunshine.Kicherer@web.de>
2015-06-17 14:18:48 +0200

    Add final content to home.php

--- Website/home.php
+++ Website/home.php
@@ -1,31 +1,43 @@
+<meta charset="UTF-8">
+
 <table>
-	  <tr><td width="400"><img src="C:\Users\Public\Pictures\Sample Pictures\Blume.jpg" align="left" width="320" height="240"></img></td>
-		  <td colespan="2" width="500"><h1>Herzlich Willkommen bei Skymap Photography</h1>
-		  <p>
-		  <h4>Kostenloser Download von hochwertigen Bildern bietet Ihnen Skymap Photography. Für jeden Anlass das passende Bild finden Sie hier in der Galerie.</h4>
-		  </td>
-		  <td>----- Facebook, Twitter & Co einfügen</td></tr>
-	  <tr><td><h2>Ihr zuverlässigster Anbieter</h2></td>
-	      <td></td>
-		  <td><h2>Aktuelles:</h2></td></tr>
-	  <tr><td align="top">
-	          <p>Über 5000 Kunden von Skymap Photography sprechen für sich. Steigen auch Sie noch heute ein und bestellen Sie den kostenlosen Skymap-Newsletter.
-			     Dieser informiert Sie stets über aktuelle Entwicklungen und die neuesten Motvie und ist selbstverständlich jederzeit abbestellbar.</p> 
-			  <p>Geben Sie hier ihre Mailadresse ein:</p>
-			  <form action="index.php." method="post"><input type="text" name="mail" width="300"/>
-			                                          <input type="submit" value="Jetzt Newsletter anfordern"/>
-			  </form> 
-	          <br><br><br>
-			  
-	          <p>Können Sie ein bestimmtes Motiv nicht finden? Dann schreiben Sie uns und entdecken Sie hier Ihr neues Traummotiv.</p>
-			  <p><strong>Jetzt Kontaktformular anfordern</strong></p></td>
-		  <td><img scr="C:\Users\Public\Pictures\Sample Pictures\Blume.jpg" align="middle" width="300"/></td>
-		  <td><p><h4>Neue Suchfunktionen!</h4>
-			     <p>Nutzen Sie Skymap Photography noch intensiver. Mit der verbesserten Suchfunktion finden Sie Ihre gewünschten Bilder im Handumdrehen.
-				 <p>Geben Sie hier Suchmotiv ein:
-				 <form action="index.php" method="post"><input type="text" name="suche" width="300"/><input type="submit" value="Suche starten"/>
-				 </form>
-			     <h4>Jetzt schon über 100 neue Bilder!</h4>
-			     <p>Nutzen Sie das berauschende Farbspiel, wirkungsvolle Motive, das pulsierende Leben für Ihre Zwecke!
-				    Laden Sie Fotos herunter und verschönern Sie Ihr Umfeld mit Skymap Photography.</p></td></tr>
-</table>		  
+	<tr>    <td width="340">
+	            <img src="images/Blume.jpg" width="320" height="290"></img></td>
+		    <td width="490">		  
+		        <h1>Herzlich Willkommen bei Skymap Photography</h1>
+		        <h2>Ihr zuverl&aumlssigster Partner f&uumlr Fotoshootings, Eventfotografie & Passbilder</h2>
+		        <h4>Wir k&oumlnnen noch mehr. Neben professioneller Bildbearbeitung bietet Skymap Photography Ihnen kostenlose Downloads von hochwertigen Fotographien an.</h4></td>
+		    <td>----- hier: Facebook, Twitter & Co</td>
+	</tr>
+
+
+	<tr>    <td>
+	            <p>&Uumlber 500 Kunden von Skymap Photography sprechen f&uumlr sich. Steigen auch Sie noch heute ein und bestellen Sie den kostenlosen Skymap-Newsletter.</p>
+		        <p>Dieser informiert Sie stets &uumlber aktuelle Entwicklungen und die neuesten Motvie und ist selbstverst&aumlndlich jederzeit abbestellbar.</p> 
+		        <p>Geben Sie hier ihre Mailadresse ein:</p>
+		        <form action="home.php." method="post">  <input type="text" name="mail" width="300"/>
+			                                             <input type="submit" value="Jetzt Newsletter anfordern"/>
+		        </form></td> 			  	          
+		    <td align="right">
+			    <img src="images/Suche.jpg" width="150" height="240"></img></td>				
+		    <td>
+			    <p><h2>Aktuelles:</h2>
+		        <h4>Neue Suchfunktionen!</h4>
+			    Nutzen Sie Skymap Photography noch intensiver. Mit der verbesserten Suchfunktion schneller zum Ziel.
+				<p>Geben Sie hier Ihr Suchmotiv ein:</p>
+				<form action="index.php" method="post"><input type="text" name="suche" width="300"/><input type="submit" value="Suche starten"/>
+				</form></td></tr>
+				
+				
+	<tr>    <td>
+	            <p>K&oumlnnen Sie ein bestimmtes Motiv nicht finden? Dann schreiben Sie uns und entdecken Sie hier Ihr neues Traummotiv.</p>
+		        <form action="home.php"><input type="submit" value="Jetzt Kontaktformular anfordern"></td>		  
+		    <td align="left">
+			    <img src="images/Person.jpg" width="150" height="200"></img></td>
+		  
+		    <td>
+		        <h4>Jetzt schon &uumlber 100 neue Bilder!</h4>
+		        <p>Nutzen Sie das berauschende Farbspiel, wirkungsvolle Motive, das pulsierende Leben für Ihre Zwecke!
+		        Laden Sie Fotos herunter und versch&oumlnern Sie Ihr Umfeld mit Skymap Photography.</p></td></tr>
+		  
+</table>			  
