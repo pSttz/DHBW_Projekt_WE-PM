fe073ba39c1bff68b195ff2d09a9950b6e047cf8
SKicherer <Sunshine.Kicherer@web.de>
2015-06-17 14:37:59 +0200

    Last update home.php

--- Website/home.php
+++ Website/home.php
@@ -1,8 +1,6 @@
-<meta charset="UTF-8">
-
 <table>
 	<tr>    <td width="340">
-	            <img src="images/Blume.jpg" width="320" height="290"></img></td>
+	            <img src="images/home/Blume.jpg" width="320" height="290"></img></td>
 		    <td width="490">		  
 		        <h1>Herzlich Willkommen bei Skymap Photography</h1>
 		        <h2>Ihr zuverl&aumlssigster Partner f&uumlr Fotoshootings, Eventfotografie & Passbilder</h2>
@@ -18,8 +16,9 @@
 		        <form action="home.php." method="post">  <input type="text" name="mail" width="300"/>
 			                                             <input type="submit" value="Jetzt Newsletter anfordern"/>
 		        </form></td> 			  	          
-		    <td align="right">
-			    <img src="images/Suche.jpg" width="150" height="240"></img></td>				
+		    <td><table>
+			    <td width="320" align="left"><img src="images/home/Person.jpg" width="150" height="240"></img></td>
+                <td align="right"><img src="images/home/Suche.jpg" width="150" height="240"></img></td> </table></td>				
 		    <td>
 			    <p><h2>Aktuelles:</h2>
 		        <h4>Neue Suchfunktionen!</h4>
@@ -33,11 +32,11 @@
 	            <p>K&oumlnnen Sie ein bestimmtes Motiv nicht finden? Dann schreiben Sie uns und entdecken Sie hier Ihr neues Traummotiv.</p>
 		        <form action="home.php"><input type="submit" value="Jetzt Kontaktformular anfordern"></td>		  
 		    <td align="left">
-			    <img src="images/Person.jpg" width="150" height="200"></img></td>
+			    <img src="images/home/Bsp.jpg" width="470" height="100"></img></td>
 		  
 		    <td>
 		        <h4>Jetzt schon &uumlber 100 neue Bilder!</h4>
-		        <p>Nutzen Sie das berauschende Farbspiel, wirkungsvolle Motive, das pulsierende Leben für Ihre Zwecke!
+		        <p>Nutzen Sie das berauschende Farbspiel, wirkungsvolle Motive, das pulsierende Leben f&uumlr Ihre Zwecke!
 		        Laden Sie Fotos herunter und versch&oumlnern Sie Ihr Umfeld mit Skymap Photography.</p></td></tr>
 		  
 </table>			  
