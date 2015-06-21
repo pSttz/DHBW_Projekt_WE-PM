bce1cfd790d50630570471fb8082f2ab40730bbf
cmmdmx <m@ixdee.de>
2015-06-21 14:34:16 +0200

    Test-Commit
    kontaktformular / über uns, test-änderung für flex-box menu und header
    (optimierung und unterstützung für safari)

---
+++ Website/contact.php
@@ -0,0 +1,29 @@
+﻿<table id="aboutus">
+	<tr>
+		<td>
+			<h1>Über uns & Kontakt</h1>
+			<h3>Über SKYMAP</h3>
+			--todo text einfügen--
+			<h3>Kontakt</h3>
+			Sie haben Fragen, Anregungen oder Wünsche an uns, unsere Produkte oder unsere Website? Treten Sie mit uns in Verbindung! 
+		</td>
+		
+		<td>
+			<div id="mailform">
+				<div>
+					<h3>Kontaktformular</h3>
+					<h4>Vorname</h4>
+					<input type="text" name="prename" placeholder="Max"/>
+					<h4>Nachname</h4>
+					<input type="text" name="name" placeholder="Mustermann"/>
+					<h4>Email</h4>
+					<input type="text" name="email" placeholder="max.mustermann@muster.de"/>
+					<h4>Anliegen</h4>
+					<textarea name="content" placeholder="Hallo, ich möchte mich über etwas informieren!"></textarea>
+				</div>
+			</div>
+		</td>
+	</tr>
+</table>
+
+
