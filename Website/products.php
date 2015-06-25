a967609a9ff2283c7ae1ffdd9b147926bb006c35
pSttz <patrick-stuetz@gmx.de>
2015-06-25 12:03:50 +0200

    Product page layout update

--- Website/products.php
+++ Website/products.php
@@ -10,161 +10,165 @@
 //-->
 </script>
 <table id="product" >
-<tr>
-<td colspan="2">
-<h1>Angebote</h1>
-</td>
-</tr>
-<tr>
-<td width="70%">	
-<h2>Fotostudio</h2>
-<p class="slogan">Ob für sich selbst oder als Geschenk, bei SKYMAP werden Ihre individuelle Fotowünsche umgesetzt.
-Unsere Leistungen:</p>
-<br>
-<br>
-<div class="product">
-<div  onclick="toggle_visibility('passbilder');" class="productlink">&raquo; Passbilder</div>
-	<div id="passbilder" style='display:none;'>
+	<tr>
+		<td colspan="2">
+			<h1>Angebote</h1>
+		</td>
+	</tr>
+	<tr>
+		<td width="65%">	
+			<h2>Fotostudio</h2>
+			<p class="slogan">Ob für sich selbst oder als Geschenk, bei SKYMAP werden Ihre individuelle Fotowünsche umgesetzt.<br>
+			Unsere Leistungen:</p>
+		</td>
+		<td>
+			<h2>Bestellungen</h2>
+			<p class="slogan">Sollten Sie sich noch nicht sicher sein, testen Sie eines unserer drei kostenlosen Produkten. Wir liefern sie direkt zu Ihnen nach Hause!</p>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<div class="product">
+				<div onclick="toggle_visibility('passbilder');" class="productlink">&raquo; Passbilder</div>
+				<div id="passbilder" style='display:none;'>
 		
-		<table class="linkContent">
-			<tr>
-				<td class="tableLeft">
-					<p class="focus">Biometrische Passbilder (4 St&uuml;ck)</p>
-					<p>- Dauer: 15-20 Minuten, inklusive kurzem Beratungsgespr&auml;ch
-					</p>
-				</td>	
+					<table class="linkContent">
+						<tr>
+							<td class="tableLeft">
+								<p class="focus">Biometrische Passbilder (4 St&uuml;ck)</p>
+								<p>- Dauer: 15-20 Minuten, inklusive kurzem Beratungsgespr&auml;ch
+								</p>
+							</td>	
 			
-				<td class="prize" dir="rtl">
-					<p>&euro; 12,00 </p>
-				</td>
-			</tr>
-			<tr>
-				<td class="tableLeft">
-					<p class="focus">Biometrische Passbilder (6 St&uuml;ck)</p>
-					<p>- Dauer: 15-20 Minuten, inklusive kurzem Beratungsgespr&auml;ch</p>
-				</td>	
-				<td class="prize" dir="rtl">
-					 <p>&euro; 15,00</p>
-				</td>
-			</tr>
-		</table>
-	</div>
-	</div>
+							<td class="prize" dir="rtl">
+								<p>&euro; 12,00 </p>
+							</td>
+						</tr>
+						<tr>
+							<td class="tableLeft">
+								<p class="focus">Biometrische Passbilder (6 St&uuml;ck)</p>
+								<p>- Dauer: 15-20 Minuten, inklusive kurzem Beratungsgespr&auml;ch</p>
+							</td>	
+							<td class="prize" dir="rtl">
+								 <p>&euro; 15,00</p>
+							</td>
+						</tr>
+					</table>
+				</div>
+			</div>
 	
-<br>
-<div class="product">
-<div onclick="toggle_visibility('fotoshootings');" class="productlink">&raquo; Fotoshootings</div>
-  	<div id="fotoshootings" style="display:none;">
- 		<table class="linkContent">
- 			<tr colspan="2">
- 				<td class="focus">Kleines Fotoshooting</td>
- 			</tr>
- 			<tr>
- 				<td class="tableleft">
- 				<p>
- 					- Dauer: 1 Stunde, inklusive Beratungsgespr&auml;ch<br>
- 					- 2 verschiedene Outfits<br>
- 					- Auf Wunsch auch Outdoor-Shooting<br>
- 					- Gemeinsames Erarbeiten des optimalen Fotos<br>
- 					- permanente Begutachtung der Fotos am Bildschirm<br>
- 					- inklusive aller Fotos in hoher Auflösung auf einer DVD<br>
- 					- 3 Top-Fotos werden digital optimiert und auf Wunsch in Schwarz-Wei&szlig; umgewandelt<br>
- 					- kleine Hautirritationen werden wegretuschiert (z.B. Pickel, Herpesbläschen)<br>
- 					- Nutzungsrechte für Facebook, Instagram und andre Social-Media-Plattformen<br>
- 				</p>
- 				</td>
- 				<td class="prize">
-	 				<p>150,00 &euro;</p>
- 				</td>
- 			</tr>
- 			<tr colspan="2">
- 				<td class="focus">Gro&szlig;es Fotoshooting</td>
- 			</tr>
- 			<tr>
- 				<td class="tableLeft">
- 					- Dauer: 2 Stunden, inklusive Beratungsgespr&auml;ch<br>
- 					- 4 verschiedene Outfits<br>
- 					- Auf Wunsch auch Outdoor-Shooting<br>
- 					- Gemeinsames Erarbeiten des optimalen Fotos<br>
- 					- Permanente Begutachtung der Fotos am Bildschirm<br>
- 					- inklusive aller Fotos in hoher Auflösung auf einer DVD<br>
- 					- 6 Top-Fotos werden digital optimiert und auf Wunsch in Schwarz-Wei&szlig; umgewandelt<br>
- 					- kleine Hautirritationen werden Wegretuschiert (z.B. Pickel, Herpesbläschen)<br>
- 					- Nutzungsrechte für Facebook, Instagram und andere Social-Media-Plattformen<br>
- 				</td>
- 				<td class="prize">
- 					<p>250,00 &euro; </p>
- 				</td>
- 			</tr>
- 		</table>
+			<br>
+			<div class="product">
+			<div onclick="toggle_visibility('fotoshootings');" class="productlink">&raquo; Fotoshootings</div>
+			  	<div id="fotoshootings" style="display:none;">
+			 		<table class="linkContent">
+			 			<tr colspan="2">
+			 				<td class="focus">Kleines Fotoshooting</td>
+			 			</tr>
+			 			<tr>
+			 				<td class="tableleft">
+			 				<p>
+			 					- Dauer: 1 Stunde, inklusive Beratungsgespr&auml;ch<br>
+			 					- 2 verschiedene Outfits<br>
+			 					- Auf Wunsch auch Outdoor-Shooting<br>
+			 					- Gemeinsames Erarbeiten des optimalen Fotos<br>
+			 					- permanente Begutachtung der Fotos am Bildschirm<br>
+			 					- inklusive aller Fotos in hoher Auflösung auf einer DVD<br>
+			 					- 3 Top-Fotos werden digital optimiert und auf Wunsch in Schwarz-Wei&szlig; umgewandelt<br>
+			 					- kleine Hautirritationen werden wegretuschiert (z.B. Pickel, Herpesbläschen)<br>
+			 					- Nutzungsrechte für Facebook, Instagram und andre Social-Media-Plattformen<br>
+			 				</p>
+			 				</td>
+			 				<td class="prize">
+				 				<p>150,00 &euro;</p>
+			 				</td>
+			 			</tr>
+			 			<tr colspan="2">
+			 				<td class="focus">Gro&szlig;es Fotoshooting</td>
+			 			</tr>
+			 			<tr>
+			 				<td class="tableLeft">
+			 					- Dauer: 2 Stunden, inklusive Beratungsgespr&auml;ch<br>
+			 					- 4 verschiedene Outfits<br>
+			 					- Auf Wunsch auch Outdoor-Shooting<br>
+			 					- Gemeinsames Erarbeiten des optimalen Fotos<br>
+			 					- Permanente Begutachtung der Fotos am Bildschirm<br>
+			 					- inklusive aller Fotos in hoher Auflösung auf einer DVD<br>
+			 					- 6 Top-Fotos werden digital optimiert und auf Wunsch in Schwarz-Wei&szlig; umgewandelt<br>
+			 					- kleine Hautirritationen werden Wegretuschiert (z.B. Pickel, Herpesbläschen)<br>
+			 					- Nutzungsrechte für Facebook, Instagram und andere Social-Media-Plattformen<br>
+			 				</td>
+			 				<td class="prize">
+			 					<p>250,00 &euro; </p>
+			 				</td>
+			 			</tr>
+			 		</table>
 
 
- 	</div>
- 	</div>
-<br>
+			 	</div>
+			 	</div>
+			<br>
 
-<div class="product">
-<div onclick="toggle_visibility('eventfoto');" class="productlink">&raquo; Eventfotografie</div>
-	<div id="eventfoto" style="display: none;">
- 		<table class="linkContent">
- 			<tr colspan="2">
- 				<td class="focus">Eventpaket</td>
- 			</tr>
- 			<tr>
- 				
- 				<td class="tableLeft">
- 					- 8 Stunden Eventfotografie, inklusive aller Fotos auf DVD<br>
- 					- Reportage, ca. 1.500 Fotos<br>
- 					- inklusive 50 High-End-Fotobearbeitungen von 25 Motiven<br>
- 					- Anreise bis zu 50km ab meinem Studio frei, danach 0,95 &euro; pro Kilometer<br>
- 					- Verlängerungsstunde: 150,00 &euro;<br>
- 				</td>
- 				<td class="prize">
- 					<p>1.500,00 &euro;</p>
- 				</td>
- 			</tr>
- 		</table>
+			<div class="product">
+			<div onclick="toggle_visibility('eventfoto');" class="productlink">&raquo; Eventfotografie</div>
+				<div id="eventfoto" style="display: none;">
+			 		<table class="linkContent">
+			 			<tr colspan="2">
+			 				<td class="focus">Eventpaket</td>
+			 			</tr>
+			 			<tr>
+			 				
+			 				<td class="tableLeft">
+			 					- 8 Stunden Eventfotografie, inklusive aller Fotos auf DVD<br>
+			 					- Reportage, ca. 1.500 Fotos<br>
+			 					- inklusive 50 High-End-Fotobearbeitungen von 25 Motiven<br>
+			 					- Anreise bis zu 50km ab meinem Studio frei, danach 0,95 &euro; pro Kilometer<br>
+			 					- Verlängerungsstunde: 150,00 &euro;<br>
+			 				</td>
+			 				<td class="prize">
+			 					<p>1.500,00 &euro;</p>
+			 				</td>
+			 			</tr>
+			 		</table>
 
-	</div>
-</div>
+				</div>
+			</div>
 
-<br>
-<form target="_blank" class="productbuttons" action="download.php?file=skymap_fotostudio.pdf" method="POST">
-   	<input type="submit" value="Preisliste PDF-Flyer">
-</form>
-<form class="productbuttons"action="?p=contact" method="POST">
-   	<input   type="submit" value="Kontaktieren">
-</form>
-<!--
-<h3>Business-Kunden</h3>
-<p class="slogan">Zeit für ein neues Image? Wir bieten Ihnen profesionelle Hilfe bei der Gestaltung von Medien für Ihr Unternehmen.<br>
-Bitte kontaktieren Sie uns und wir werden gemeinsam Ihre Ziele und W&uuml;nsche umsetzen.
-</p>
-<form class="productbuttons"action="?p=contact" method="POST">
-   	<input   type="submit" value="Kontaktieren">
+			<br>
+			<form target="_blank" class="productbuttons" action="download.php?file=skymap_fotostudio.pdf" method="POST">
+			   	<input type="submit" value="Preisliste PDF-Flyer">
+			</form>
+			<form class="productbuttons"action="?p=contact" method="POST">
+			   	<input   type="submit" value="Kontaktieren">
+			</form>
+			<!--
+			<h3>Business-Kunden</h3>
+			<p class="slogan">Zeit für ein neues Image? Wir bieten Ihnen profesionelle Hilfe bei der Gestaltung von Medien für Ihr Unternehmen.<br>
+			Bitte kontaktieren Sie uns und wir werden gemeinsam Ihre Ziele und W&uuml;nsche umsetzen.
+			</p>
+			<form class="productbuttons"action="?p=contact" method="POST">
+			   	<input   type="submit" value="Kontaktieren">
 
-</form>
--->
-</td>
-<td>
-<h2>Bestellungen</h2>
-<p class="slogan">Sollten Sie sich noch nicht sicher sein, testen Sie eines unserer drei kostenlosen Produkten. Wir liefern sie direkt zu Ihnen nach Hause!</p>
-<div id="order">
-	<form method="POST" >
-		<h4>Vorname:</h4><input type="text" name="name" placeholder="Max" onblur="validateInput(this)">
-		<h4>Nachname:</h4><input type="text" name="surname" placeholder="Mustermann">
-		<h4>Adresse:</h4><input type="text" name="street" placeholder="Musterstraße 12">
-		<h4>Postleitzahl:</h4><input type="text" name="pCode" placeholder="12345">
-		<h4>Ort:</h4><input type="text" name="town" placeholder="Musterstadt">
-		<h4>Produkte:</h4><select size="1" name="product">
-			<option>Produkt 1</option>
-			<option>Produkt 2</option>
-			<option>Produkt 3</option>
-		</select>
-		<input type="button" value="Kostenlos Bestellen" name="send">
-	</form>
+			</form>
+			-->
+		</td>
+		<td>
+			<div id="order">
+				<form method="POST" >
+					<h4>Vorname:</h4><input type="text" name="name" placeholder="Max" onblur="validateInput(this)">
+					<h4>Nachname:</h4><input type="text" name="surname" placeholder="Mustermann">
+					<h4>Adresse:</h4><input type="text" name="street" placeholder="Musterstraße 12">
+					<h4>Postleitzahl:</h4><input type="text" name="pCode" placeholder="12345">
+					<h4>Ort:</h4><input type="text" name="town" placeholder="Musterstadt">
+					<h4>Produkte:</h4><select size="1" name="product">
+						<option>Produkt 1</option>
+						<option>Produkt 2</option>
+						<option>Produkt 3</option>
+					</select>
+					<input type="button" value="Kostenlos Bestellen" name="send">
+				</form>
 
-</div>
-</td>
-</tr>
+			</div>
+		</td>
+	</tr>
 </table>
