7a79bbc29631a82ac0c6bdde2b0c4e63020f9f03
pSttz <patrick-stuetz@gmx.de>
2015-07-07 22:42:45 +0200

    Update add-images.php

--- Website/admin/add-images.php
+++ Website/admin/add-images.php
@@ -1,14 +1,11 @@
 <?php 
-
 	$db = mysqli_connect("localhost", "skymap", "u6&bNl58", "skymap");
 	$db->set_charset("utf8");
-
 	// check connection
 	if (mysqli_connect_errno()) {
 	    printf("Connect failed: %s\n", mysqli_connect_error());
 	    exit();
 	}
-
 	$newImages = $_GET['newImages'];
 	
 	$title = $newImages["title"];
@@ -16,6 +13,5 @@
 	$description = $newImages["description"];
 	$tags = $newImages["tags"];
 	$downloadable = $newImages["downloadable"];
-
 	mysqli_query($db, "INSERT INTO `skymap`.`galerie` (`id`, `title`, `href`, `description`, `date`, `tags`, `downloadable`, `likes`) VALUES (NULL, '$title', '$href', '$description', NOW(), '$tags', '$downloadable', '0')");
 ?>
